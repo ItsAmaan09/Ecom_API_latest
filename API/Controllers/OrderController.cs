bb16427300ad45b4ece8bb039c985e5057b6d098
Mohammed amaan <amaanvisiontech@gmail.com>
2024-05-21 18:53:14 +0530

    commit to orderController

--- API/Controllers/OrderController.cs
+++ API/Controllers/OrderController.cs
@@ -22,35 +22,67 @@ namespace API.Controllers
 		{
 			try
 			{
-				var orders = _orderManager.GetAllOrders(Status);
-				return Ok(orders);
+				var response = _orderManager.GetAllOrders(Status);
+				return Ok(response);
 			}
 			catch (System.Exception ex)
 			{
 				return BadRequest(ex.Message);
 			}
 		}
+
 		[HttpGet("{id}")]
 		public IActionResult GetOrderDetails(int id)
 		{
 			try
 			{
-				var order = _orderManager.GetOrderDetails(id);
-				return Ok(order);
+				var response = _orderManager.GetOrderDetails(id);
+				return Ok(response);
 			}
 			catch (System.Exception ex)
 			{
 				return BadRequest(ex.Message);
 			}
 		}
+
 		[HttpPost]
 		[Route("Create")]
 		public IActionResult CreateOrder(OrderDTO orderDTO)
 		{
 			try
 			{
-				var result = _orderManager.CreateOrder(orderDTO);
-				return Ok(result);
+				var response = _orderManager.CreateOrder(orderDTO);
+				return Ok(response);
+			}
+			catch (System.Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+		[HttpPost]
+		[Route("{id}/Confirm")]
+		public IActionResult ConfirmOrder(int id)
+		{
+			try
+			{
+				var response = _orderManager.ConfirmOrder(id);
+				return Ok(response);
+			}
+			catch (System.Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+		[HttpPost]
+		[Route("{id}/status")]
+		public IActionResult UpdateOrderStatus(int id, OrderStatusDTO orderStatusDTO)
+		{
+			try
+			{
+				var response = _orderManager.UpdateOrderStatus(id, orderStatusDTO.Status);
+				return Ok(response);
 			}
 			catch (System.Exception ex)
 			{
